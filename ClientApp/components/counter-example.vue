f7d2b858bfad342c5dd0e7d0bd4651c7580a3f4a
erw13n <erw13n@gmail.com>
2017-05-18 06:42:37 +0700

    Simple Vuex Implementation (#34)
    * Fix publish issue & Compress vendor css on publish
    Closes #6
    
    * Simple Vuex implement

--- ClientApp/components/counter-example.vue
+++ ClientApp/components/counter-example.vue
@@ -1,42 +1,56 @@
-<template>
-    <div>
-        <h1>Counter</h1>
+﻿<template>
+  <div>
+    <h1>Counter</h1>
 
-        <p>This is a simple example of a Vue.js component.</p>
+    <p>This is a simple example of a Vue.js component & Vuex</p>
 
-        <p>Current count: <strong>{{ currentCount }}</strong></p>
-        <p>Auto count: <strong>{{ autoCount }}</strong></p>
+    <p>
+      Current count (Vuex): <strong>{{ currentCount }}</strong>
+    </p>
+    <p>
+      Auto count: <strong>{{ autoCount }}</strong>
+    </p>
 
-        <button @click="incrementCounter()">Increment</button>
-        <button @click="resetCounter()">Reset</button>
-    </div>
+    <button @click="incrementCounter()">Increment</button>
+    <button @click="resetCounter()">Reset</button>
+  </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            currentCount: 0,
-            autoCount: 0,
-        }
-    },
+  import { mapActions, mapState } from 'vuex'
 
-    methods: {
-        incrementCounter: function() {
-            this.currentCount++;
-        },
-        resetCounter: function() {
-            this.currentCount = 0;
-            this.autoCount = 0;
-        }
-    },
+  export default {
+  data() {
+  return {
+  autoCount: 0,
+  }
+  },
 
-    created() {
-        setInterval(() => {
-            this.autoCount += 1
-        }, 1000)
-    }
-}
+  computed: {
+  ...mapState({
+  currentCount: state => state.counter
+  })
+  },
+
+  methods: {
+  ...mapActions(['setCounter']),
+
+  incrementCounter: function() {
+  var counter = this.currentCount + 1;
+  this.setCounter({counter: counter});
+  },
+  resetCounter: function() {
+  this.setCounter({counter: 0});
+  this.autoCount = 0;
+  }
+  },
+
+  created() {
+  setInterval(() => {
+  this.autoCount += 1
+  }, 1000)
+  }
+  }
 </script>
 
 <style>
--- ClientApp/store/index.js
+++ ClientApp/store/index.js
@@ -1,13 +1,32 @@
-import Vue from 'vue'
+﻿import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const INITIAL_STATE = {
-    loggedIn: false,
-    loggedInUser: {}
-};
+// TYPES
+const MAIN_SET_COUNTER = 'MAIN_SET_COUNTER'
+
+// STATE
+const state = {
+    counter: 0
+}
+
+// MUTATIONS
+const mutations = {
+    [MAIN_SET_COUNTER](state, obj) {
+        state.counter = obj.counter
+    }
+}
+
+// ACTIONS
+const actions = ({
+    setCounter({ commit }, obj) {
+        commit(MAIN_SET_COUNTER, obj)
+    }
+})
 
 export default new Vuex.Store({
-    INITIAL_STATE
+    state,
+    mutations,
+    actions
 });
