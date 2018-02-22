9a38ed2b8c8f0face21600a5b890ef96eedcfe09
Nordes Lamarre <nordes@gmail.com>
2018-02-22 21:27:39 +0900

    Fixed spacing issue.

--- content/Models/WeatherForecast.cs
+++ content/Models/WeatherForecast.cs
@@ -1,17 +1,17 @@
 namespace Vue2Spa.Models
 {
     public class WeatherForecast
-  {
-      public string DateFormatted { get; set; }
-      public int TemperatureC { get; set; }
-      public string Summary { get; set; }
+    {
+        public string DateFormatted { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
 
-      public int TemperatureF
-      {
-          get
-          {
-              return 32 + (int)(TemperatureC / 0.5556);
-          }
-      }
-  }
+        public int TemperatureF
+        {
+            get
+            {
+                return 32 + (int)(TemperatureC / 0.5556);
+            }
+        }
+    }
 }
