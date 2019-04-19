634dcdf3519f77c7a0e9198127bc7ae58e2622d0
Mark Pieszak <hello@trilon.io>
2019-04-19 15:01:29 -0400

    fix(font-awesome): fix font-awesome requires to import to reduce main bundle size
    reduces from 1+mb to 81kb
    
    fixes: #126

--- content/ClientApp/icons.js
+++ content/ClientApp/icons.js
@@ -2,21 +2,25 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 // Official documentation available at: https://github.com/FortAwesome/vue-fontawesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+import { faEnvelope, faHeart, faGraduationCap, faHome, faInfo, faList, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome, faMicrosoft, faVuejs } from '@fortawesome/free-brands-svg-icons'
+
 // If not present, it won't be visible within the application. Considering that you
 // don't want all the icons for no reason. This is a good way to avoid importing too many
 // unnecessary things.
 library.add(
-  require('@fortawesome/free-solid-svg-icons').faEnvelope,
-  require('@fortawesome/free-solid-svg-icons').faHeart,
-  require('@fortawesome/free-solid-svg-icons').faGraduationCap,
-  require('@fortawesome/free-solid-svg-icons').faHome,
-  require('@fortawesome/free-solid-svg-icons').faInfo,
-  require('@fortawesome/free-solid-svg-icons').faList,
-  require('@fortawesome/free-solid-svg-icons').faSpinner,
+  faEnvelope,
+  faHeart,
+  faGraduationCap,
+  faHome,
+  faInfo,
+  faList,
+  faSpinner,
+
   // Brands
-  require('@fortawesome/free-brands-svg-icons').faFontAwesome,
-  require('@fortawesome/free-brands-svg-icons').faMicrosoft,
-  require('@fortawesome/free-brands-svg-icons').faVuejs
+  faFontAwesome,
+  faMicrosoft,
+  faVuejs
 )
 
 export {
